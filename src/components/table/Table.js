d2a135d8c1019ce27f9272e4f09922e5e24a4c7e
ruslay <tutknigiru@yandex.ru>
2023-02-06 16:40:15 +0300

    3 stage

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,6 @@ import {createTable} from "@/components/table/table.template";
 export class Table extends ExcelComponent {
     static className = 'excel__table'
     toHTML() {
-        return createTable(100)
+        return createTable(101)
     }
 }
